515e1afa66d9ec3272413aff6d9064c6434016a0
Julian Eng <julian3ng@gmail.com>
2018-09-08 17:07:05 -0700

    Remove echoing

--- kilo.c
+++ kilo.c
@@ -1,6 +1,24 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    struct termios raw;
+    
+    // Get current terminal attributes    
+    tcgetattr(STDIN_FILENO, &raw);
+
+    // c_lflag handles local flags
+    // there are also input, output, and control flags
+    raw.c_lflag &= ~(ECHO);
+
+    // Set attributes on stdin, after stdin is flushed
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
 int main(void) {
+    enableRawMode();
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
