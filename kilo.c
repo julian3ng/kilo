490a5c0f5e38b943df59da389c760c924f1fa150
Julian Eng <julian3ng@gmail.com>
2018-09-10 07:07:46 -0700

    Add page up / down

--- kilo.c
+++ kilo.c
@@ -14,9 +14,19 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+    ARROW_LEFT  = 1000,
+    ARROW_DOWN,
+    ARROW_UP,
+    ARROW_RIGHT,
+    PAGE_UP,
+    PAGE_DOWN,
+};
+
 /*** data ***/
 
 struct editorConfig {
+    int cx, cy;
     int screenrows, screencols;
     struct termios orig_termios;
 };
@@ -34,7 +44,7 @@ void die(const char *s) {
     exit(1);
 }
 
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
     // until a char is read, read
@@ -43,7 +53,32 @@ char editorReadKey() {
             die("read");
     }
 
-    return c;
+    if (c == '\x1b') {
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+        if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                    case '5': return PAGE_UP;
+                    case '6': return PAGE_DOWN;
+                    }
+                }
+            } else {
+                switch(seq[1]) {
+                case 'D': return ARROW_LEFT;
+                case 'B': return ARROW_DOWN;
+                case 'A': return ARROW_UP;                
+                case 'C': return ARROW_RIGHT;
+                }
+            }
+        }
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -148,13 +183,46 @@ void abFree(struct abuf *ab) {
 
 /*** input ***/
 
+void editorMoveCursor(int key) {
+    switch (key) {
+    case ARROW_LEFT:
+        if (E.cx != 0) { E.cx--; }
+        break;
+    case ARROW_RIGHT:
+        if (E.cx != E.screencols - 1) { E.cx++; }
+        break;
+    case ARROW_DOWN:
+        if (E.cy != E.screenrows - 1) { E.cy++; }
+        break;
+    case ARROW_UP:
+        if (E.cy != 0) { E.cy--; }
+        break;
+    }
+}
+
 void editorProcessKeypress() {
-    char c = editorReadKey();
+    int c = editorReadKey();
     switch (c) {
     case CTRL_KEY('q'):
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
+        break;
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        int times = E.screenrows;
+        while (times--)
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+
+    break;
+        
+    case ARROW_LEFT:
+    case ARROW_DOWN:
+    case ARROW_UP:
+    case ARROW_RIGHT:
+        editorMoveCursor(c);
         break;
     }
 }
@@ -195,7 +263,9 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[H", 3);
     editorDrawRows(&ab);
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6);
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
@@ -205,6 +275,8 @@ void editorRefreshScreen() {
 /*** init ***/
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWindowSize");
 }
